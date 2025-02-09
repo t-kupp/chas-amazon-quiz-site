ff6bcda09a1c7d3b99354ad13838018f38b1ca20
Justus <jurre@pop-os.localdomain>
2025-02-09 20:58:09 +0100

    added localstorage functionality

--- context/quizContext.jsx
+++ context/quizContext.jsx
@@ -1,11 +1,27 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { initialQuizData } from './initialQuizData';
 
 export const QuizContext = createContext();
 
+
 export default function QuizProvider({ children }) {
   const [quizData, setQuizData] = useState(initialQuizData);
   const [selectedQuiz, setSelectedQuiz] = useState();
+
+
+  useEffect(() => {
+    const storedQuizzes = JSON.parse(localStorage.getItem('quizzes'));
+    if (storedQuizzes) {
+      setQuizData(storedQuizzes);
+    }
+  }, []);
+  
+  useEffect(() => {
+    if (quizData.length > 0) {
+      localStorage.setItem('quizzes', JSON.stringify(quizData));
+    }
+  }, [quizData]);
+  
 
   return <QuizContext.Provider value={{ quizData, selectedQuiz, setSelectedQuiz, setQuizData }}>{children}</QuizContext.Provider>;
 }
