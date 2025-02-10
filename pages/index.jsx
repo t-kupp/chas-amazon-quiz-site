6628cd57ff7ab9005ab0d070ab7ba0c956476442
t-kupp <kupper.thorge@gmail.com>
2025-02-10 13:58:18 +0100

    increased font size and small UI adjustments

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,11 +8,11 @@ export default function Home() {
   return (
     <div className='hero min-h-screen bg-base-200'>
       <div className='hero-content text-center'>
-        <div className='max-w-xl'>
+        <div className='max-w-3xl'>
           <div className='rounded-2xl bg-base-100 px-4 py-4 shadow-lg lg:px-16 lg:py-8'>
             <img src='/oscars-logo.svg' alt='Oscar Logo' className='mx-auto mb-4 max-w-48' />
             <h1 className='text-3xl font-semibold lg:text-5xl'>Welcome to the Oscars Quiz!</h1>
-            <p className='py-10'>
+            <p className='py-10 text-lg'>
               This is Group Amazon's quiz site.
               <br /> Are you ready to test your knowledge of the famous Academy Awards?
             </p>
@@ -22,7 +22,7 @@ export default function Home() {
                   <Link key={key} href={quiz.href}>
                     <button
                       onClick={() => setSelectedQuiz(quiz)}
-                      className='btn btn-primary w-full font-normal tracking-wide'
+                      className='btn btn-primary w-full text-base font-normal'
                     >
                       {quiz.title}
                     </button>
